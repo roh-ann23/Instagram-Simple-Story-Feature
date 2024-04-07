5818a062d0e094e58beb3f7efe1b0641caa35fa0
roh-ann23 <158121284+roh-ann23@users.noreply.github.com>
2024-04-07 19:37:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+
+let arr = [
+    {dp:"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story :"https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D"},
+
+    {dp:"https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story :"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+    {dp:"https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9kZWx8ZW58MHx8MHx8fDA%3D",
+    story :"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+    {dp:"https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story :"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+    {dp:"https://media.istockphoto.com/id/1153386427/photo/beauty-portrait-brunette-woman-with-tanned-skin-and-brown-eyes-and-fresh-healthy-skin.webp?b=1&s=170667a&w=0&k=20&c=GkOQmxUORfJoi8M_uplKMBF4N4eZpnoMVrQgkNe_jJo=",
+    story :"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+    {dp:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+    story :"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+]
+
+
+
+var storiyan = document.querySelector(".storiyan")
+var clutter = "";
+
+arr.forEach(function(val,idx){
+    clutter += `<div class="story">
+    <img id="${idx}"src="${val.dp}" alt="">
+</div>`
+})
+
+storiyan.innerHTML = clutter;
+
+
+let fullScreen = document.querySelector(".full_screen")
+storiyan.addEventListener("click",function(e){
+    fullScreen.style.display = "block";
+    fullScreen.style.backgroundImage= `url(${arr[e.target.id].story})`;
+
+    setTimeout(function(){
+        fullScreen.style.display = "none";
+    },2000)
+
+})
+
+
+
+
+// MI kela ahe ugach kah trii
+let love = document.querySelector(".icon1");
+let cmt =  document.querySelector(".icon2");
+
+let flag = 0;
+icon.addEventListener("click",function(){
+    if(flag==0){
+        love.style.color = "red";
+        flag = 1;
+    }
+    else{
+        love.style.color = "white";
+        flag = 0;
+    }
+})
